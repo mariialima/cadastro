69874c56d37bab298c0dfa8416ae0bb375ce0e3d
mariialima <mariiaeduardafl@gmail.com>
2023-03-22 08:48:26 -0300

    ceudebrigadeiro

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//ANIMAÇÃO DO BOTAO DE LOGIN//
+
 var formSignin = document.querySelector('#signin')
 var formSignup = document.querySelector('#signup')
 var btnColor = document.querySelector('.btnColor')
@@ -7,11 +9,25 @@ document.querySelector('#btnSignin')
     formSignin.style.left = "25px"
     formSignup.style.left = "450px"
     btnColor.style.left = "0px"
-})
+  })
 
 document.querySelector('#btnSignup')
   .addEventListener('click', () => {
     formSignin.style.left = "-450px"
     formSignup.style.left = "25px"
     btnColor.style.left = "110px"
-})
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
